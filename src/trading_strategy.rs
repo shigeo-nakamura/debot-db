a152c976bce9ddb362a437160bb19ad533859bbb
Shigeo Nakamura <snakamura@cinemo.com>
2025-03-30 10:52:42 +0200

    Remove the strategies other than Inago

--- src/trading_strategy.rs
+++ src/trading_strategy.rs
@@ -9,9 +9,6 @@ pub enum TrendType {
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Serialize, Deserialize)]
 pub enum TradingStrategy {
-    MeanReversion(TrendType),
-    RandomMeanReversion(TrendType),
-    Grid(TrendType),
     Inago(TrendType),
     RandomInago(TrendType),
 }
@@ -19,11 +16,7 @@ pub enum TradingStrategy {
 impl TradingStrategy {
     pub fn trend_type(&self) -> &TrendType {
         match self {
-            TradingStrategy::RandomMeanReversion(t)
-            | TradingStrategy::MeanReversion(t)
-            | TradingStrategy::Grid(t)
-            | TradingStrategy::RandomInago(t)
-            | TradingStrategy::Inago(t) => t,
+            TradingStrategy::RandomInago(t) | TradingStrategy::Inago(t) => t,
         }
     }
 }
@@ -31,57 +24,6 @@ impl TradingStrategy {
 impl PartialEq for TradingStrategy {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (
-                TradingStrategy::MeanReversion(TrendType::Any),
-                TradingStrategy::MeanReversion(TrendType::Up),
-            )
-            | (
-                TradingStrategy::MeanReversion(TrendType::Up),
-                TradingStrategy::MeanReversion(TrendType::Any),
-            )
-            | (
-                TradingStrategy::MeanReversion(TrendType::Any),
-                TradingStrategy::MeanReversion(TrendType::Down),
-            )
-            | (
-                TradingStrategy::MeanReversion(TrendType::Down),
-                TradingStrategy::MeanReversion(TrendType::Any),
-            ) => true,
-            (TradingStrategy::MeanReversion(t1), TradingStrategy::MeanReversion(t2))
-                if t1 == t2 =>
-            {
-                true
-            }
-
-            (
-                TradingStrategy::RandomMeanReversion(TrendType::Any),
-                TradingStrategy::RandomMeanReversion(TrendType::Up),
-            )
-            | (
-                TradingStrategy::RandomMeanReversion(TrendType::Up),
-                TradingStrategy::RandomMeanReversion(TrendType::Any),
-            )
-            | (
-                TradingStrategy::RandomMeanReversion(TrendType::Any),
-                TradingStrategy::RandomMeanReversion(TrendType::Down),
-            )
-            | (
-                TradingStrategy::RandomMeanReversion(TrendType::Down),
-                TradingStrategy::RandomMeanReversion(TrendType::Any),
-            ) => true,
-            (
-                TradingStrategy::RandomMeanReversion(t1),
-                TradingStrategy::RandomMeanReversion(t2),
-            ) if t1 == t2 => true,
-
-            (TradingStrategy::Grid(TrendType::Any), TradingStrategy::Grid(TrendType::Up))
-            | (TradingStrategy::Grid(TrendType::Up), TradingStrategy::Grid(TrendType::Any))
-            | (TradingStrategy::Grid(TrendType::Any), TradingStrategy::Grid(TrendType::Down))
-            | (TradingStrategy::Grid(TrendType::Down), TradingStrategy::Grid(TrendType::Any)) => {
-                true
-            }
-            (TradingStrategy::Grid(t1), TradingStrategy::Grid(t2)) if t1 == t2 => true,
-
             (TradingStrategy::Inago(TrendType::Any), TradingStrategy::Inago(TrendType::Up))
             | (TradingStrategy::Inago(TrendType::Up), TradingStrategy::Inago(TrendType::Any))
             | (TradingStrategy::Inago(TrendType::Any), TradingStrategy::Inago(TrendType::Down))
